e1a23a7daf6051ef8442a91c9326b4004bf736f6
Martín M <skydiver@users.noreply.github.com>
2019-03-31 17:51:30 -0300

    created demo component

---
+++ components/demo/MetricCards.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-wrap">
+    <MetricCard
+      icon="fa-wallet"
+      icon-color="green-dark"
+      title="Total Revenue"
+      value="$3249"
+      arrow="up"
+      arrow-color="green"
+    />
+    <MetricCard
+      icon="fa-users"
+      icon-color="orange-dark"
+      title="Total Users"
+      value="249"
+      arrow="equal"
+      arrow-color="orange"
+    />
+    <MetricCard
+      icon="fa-user-plus"
+      icon-color="yellow-dark"
+      title="New Users"
+      value="2"
+      arrow="up"
+      arrow-color="yellow-dark"
+    />
+
+    <MetricCard
+      icon="fa-server"
+      icon-color="blue-dark"
+      title="Server Uptime"
+      value="152 days"
+    />
+    <MetricCard
+      icon="fa-tasks"
+      icon-color="indigo-dark"
+      title="To Do List"
+      value="7 tasks"
+    />
+    <MetricCard
+      icon="fa-inbox"
+      icon-color="red-dark"
+      title="Issues"
+      value="3"
+      arrow="up"
+      arrow-color="red"
+    />
+  </div>
+</template>
+
+<script>
+import MetricCard from '~/components/MetricCard.vue';
+
+export default {
+  components: {
+    MetricCard
+  }
+};
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>HELLO WORLD!</div>
+  <div>
+    <MetricCards />
+    <Divider />
+
+    <div class="flex flex-row flex-wrap flex-grow mt-2">
+      <div class="w-full md:w-1/2 p-3">
+        HELLO
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import MetricCards from '~/components/Demo/MetricCards.vue';
+import Divider from '~/components/Divider.vue';
+
+export default {
+  components: {
+    MetricCards,
+    Divider
+  }
+};
 </script>
