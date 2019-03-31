836b3a76777ca95d41f6c97cf2c60bbde6400752
Martín M <skydiver@users.noreply.github.com>
2019-03-31 18:49:41 -0300

    Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <footer class="bg-white border-t border-grey-light shadow">
+    <div class="container max-w-md mx-auto flex py-8">
+      <div class="w-full mx-auto flex flex-wrap">
+        <div class="flex w-full md:w-1/2 ">
+          <div class="px-8">
+            <h3 class="font-bold text-black">About</h3>
+            <p class="py-4 text-grey-dark text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              vel mi ut felis tempus commodo nec id erat. Suspendisse
+              consectetur dapibus velit ut lacinia.
+            </p>
+          </div>
+        </div>
+
+        <div class="flex w-full md:w-1/2">
+          <div class="px-8">
+            <h3 class="font-bold text-black">Social</h3>
+            <ul class="list-reset items-center text-sm pt-3">
+              <li>
+                <a
+                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  href="#"
+                  >Add social link</a
+                >
+              </li>
+              <li>
+                <a
+                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  href="#"
+                  >Add social link</a
+                >
+              </li>
+              <li>
+                <a
+                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  href="#"
+                  >Add social link</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,18 @@
         <nuxt />
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Nav from '~/components/Nav.vue';
+import Footer from '~/components/Footer.vue';
 
 export default {
   components: {
-    Nav
+    Nav,
+    Footer
   },
   head: {
     bodyAttrs: {
