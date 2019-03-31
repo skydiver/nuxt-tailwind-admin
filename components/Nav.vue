00dbab5de54952a2bd0013844b119e355fdedfc6
Martín M <skydiver@users.noreply.github.com>
2019-03-31 18:51:45 -0300

    added Nav component actions

--- components/Nav.vue
+++ components/Nav.vue
@@ -165,3 +165,63 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const userMenuDiv = document.getElementById('userMenu');
+    const userMenu = document.getElementById('userButton');
+
+    const navMenuDiv = document.getElementById('nav-content');
+    const navMenu = document.getElementById('nav-toggle');
+
+    document.onclick = check;
+
+    function check(e) {
+      const target = (e && e.target) || (event && event.srcElement);
+
+      // User Menu
+      if (!checkParent(target, userMenuDiv)) {
+        // click NOT on the menu
+        if (checkParent(target, userMenu)) {
+          // click on the link
+          if (userMenuDiv.classList.contains('invisible')) {
+            userMenuDiv.classList.remove('invisible');
+          } else {
+            userMenuDiv.classList.add('invisible');
+          }
+        } else {
+          // click both outside link and outside menu, hide menu
+          userMenuDiv.classList.add('invisible');
+        }
+      }
+
+      // Nav Menu
+      if (!checkParent(target, navMenuDiv)) {
+        // click NOT on the menu
+        if (checkParent(target, navMenu)) {
+          // click on the link
+          if (navMenuDiv.classList.contains('hidden')) {
+            navMenuDiv.classList.remove('hidden');
+          } else {
+            navMenuDiv.classList.add('hidden');
+          }
+        } else {
+          // click both outside link and outside menu, hide menu
+          navMenuDiv.classList.add('hidden');
+        }
+      }
+    }
+
+    function checkParent(t, elm) {
+      while (t.parentNode) {
+        if (t === elm) {
+          return true;
+        }
+        t = t.parentNode;
+      }
+      return false;
+    }
+  }
+};
+</script>
