02b70bbede5f9c1bdf5e6cc0a86535fa73752725
Martín M <skydiver@users.noreply.github.com>
2019-04-01 10:23:26 -0300

    components reorganization

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,19 @@
 </template>
 
 <script>
+import Divider from '~/components/widgets/Divider.vue';
 import BaseContainer from '~/components/containers/BaseContainer.vue';
 import MetricCards from '~/components/_demo/MetricCards.vue';
 import GraphCards from '~/components/_demo/GraphCards.vue';
 import Table from '~/components/_demo/Table.vue';
-import Divider from '~/components/layout/Divider.vue';
 
 export default {
   components: {
+    Divider,
     BaseContainer,
     MetricCards,
     GraphCards,
-    Table,
-    Divider
+    Table
   }
 };
 </script>
