521d970e7212c8bad650ffa4a8cb2cff47beb6ee
Martín M <skydiver@users.noreply.github.com>
2019-05-18 18:09:41 -0300

    updated container component

--- components/containers/BaseContainer.vue
+++ components/containers/BaseContainer.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container w-full mx-auto pt-20">
-    <div
-      class="w-full px-4 md:px-0 md:mt-8 mb-16 text-grey-darkest leading-normal"
-    >
+    <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
       <slot></slot>
     </div>
   </div>
