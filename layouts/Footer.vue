7ab6bf3115b845a548ff70c775238a0075f6a349
Martín M <skydiver@users.noreply.github.com>
2019-05-18 18:07:37 -0300

    updated footer component

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="bg-white border-t border-grey-light shadow">
+  <footer class="bg-white border-t border-gray-400 shadow">
     <div class="container max-w-md mx-auto flex py-8">
       <div class="w-full mx-auto flex flex-wrap">
         <div class="flex w-full md:w-1/2 ">
           <div class="px-8">
-            <h3 class="font-bold text-black">About</h3>
-            <p class="py-4 text-grey-dark text-sm">
+            <h3 class="font-bold font-bold text-gray-900">About</h3>
+            <p class="py-4 text-gray-600 text-sm">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
               vel mi ut felis tempus commodo nec id erat. Suspendisse
               consectetur dapibus velit ut lacinia.
@@ -15,25 +15,25 @@
 
         <div class="flex w-full md:w-1/2">
           <div class="px-8">
-            <h3 class="font-bold text-black">Social</h3>
+            <h3 class="font-bold font-bold text-gray-900">Social</h3>
             <ul class="list-reset items-center text-sm pt-3">
               <li>
                 <a
-                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
                   href="#"
                   >Add social link</a
                 >
               </li>
               <li>
                 <a
-                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
                   href="#"
                   >Add social link</a
                 >
               </li>
               <li>
                 <a
-                  class="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1"
+                  class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
                   href="#"
                   >Add social link</a
                 >
